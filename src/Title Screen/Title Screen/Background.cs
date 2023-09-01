c9d5462a6b7932328df6cc0709abc9cb4201f15c
Eugene Slipych <slipeug@ksu.edu>
2023-09-01 03:13:17 -0500

    Moving clouds added

---
+++ src/Title Screen/Title Screen/Background.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace TitleScreen
+{
+
+    public class Background
+    {
+        private Texture2D mainBackground;
+        private Texture2D clouds1;
+        private Texture2D clouds2;
+        private Texture2D clouds3;
+
+        public float Width { get; private set; }
+        public float Height { get; private set; }
+        
+        float clouds1Pos = 0;
+        float clouds2Pos = 0;
+        float clouds3Pos = 0;
+
+        public void LoadContent(ContentManager content)
+        {
+            mainBackground = content.Load<Texture2D>("1");
+            clouds1 = content.Load<Texture2D>("2");
+            clouds2 = content.Load<Texture2D>("3");
+            clouds3 = content.Load<Texture2D>("4");
+        }
+
+        public void Update (GameTime gameTime)
+        {
+            clouds1Pos = (float) Math.Cos(gameTime.TotalGameTime.TotalSeconds) * 10;
+            clouds2Pos = (float) Math.Cos(gameTime.TotalGameTime.TotalSeconds + 5) * 10;
+            clouds3Pos = (float) Math.Cos(gameTime.TotalGameTime.TotalSeconds + 10) * 10;
+        }
+
+        public void Draw (GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(mainBackground, new Vector2(0, 0), null, Color.White, 0f, new Vector2 (0,0), 2f, SpriteEffects.None, 0);
+            spriteBatch.Draw(clouds1, new Vector2(clouds1Pos, 0), null, Color.White, 0f, new Vector2(20, 30), 2.3f, SpriteEffects.None, 1);
+            spriteBatch.Draw(clouds2, new Vector2(clouds2Pos, 0), null, Color.White, 0f, new Vector2(20, 30), 2.3f, SpriteEffects.None, 0);
+            spriteBatch.Draw(clouds3, new Vector2(clouds3Pos, 0), null, Color.White, 0f, new Vector2(20, 30), 2.3f, SpriteEffects.None, 0);
+        }
+    }
+}
---
+++ src/Title Screen/Title Screen/TitleScreen.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+
+
+namespace TitleScreen
+{
+    public class TitleScreen : Game
+    {
+        public const int WINDOW_WIDTH = 1152;
+        public const int WINDOW_HEIGHT = 648;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private SpriteFont _instructions;
+        private SpriteFont _gameName;
+        private Background _background;
+
+        public TitleScreen()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _background = new Background();
+
+            // Set the desired window size here
+            _graphics.PreferredBackBufferWidth = WINDOW_WIDTH; // Width in pixels
+            _graphics.PreferredBackBufferHeight = WINDOW_HEIGHT; // Height in pixels
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _background.LoadContent(Content);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _background.Update(gameTime);
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            _background.Draw(gameTime, _spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
